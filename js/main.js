f770ba287f7c8be77e67963593a16145c5634696
unknown <seifmthabet@gmail.com>
2022-08-24 10:17:39 +0300

    uploaded

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+let countDownDate = new Date("Dec 31, 2022 23:59:59").getTime();
+let counter = setInterval(() => {
+
+  let countDownDate = new Date().getTime();
+  let dateDiff = countDownDate - countDownDate;
+  let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((dateDiff % (1000 * 60 * 60 )) / (1000 * 60 ));
+  let seconds = Math.floor((dateDiff % (1000 * 60 )) / 1000);
+
+  document.querySelector(".days").innerHTML = days;
+  document.querySelector(".hours").innerHTML = hours;
+  document.querySelector(".minutes").innerHTML = minutes;
+  document.querySelector(".seconds").innerHTML = seconds;
+
+  if (dateDiff < 0) {
+    clearInterval(counter);
+  }
+}, 1000);
